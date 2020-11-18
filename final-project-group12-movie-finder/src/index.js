802dee6bc467f43e44d504cedc2cece185f5ccee
Svi-Kos <svitlanakos@gmail.com>
2020-11-18 19:02:28 +0200

    додає функціонал пошуку по назві (get запити)

--- final-project-group12-movie-finder/src/index.js
+++ final-project-group12-movie-finder/src/index.js
@@ -31,7 +31,6 @@ mainEl.insertAdjacentHTML('beforeend', mainMarkup);
 footerEl.insertAdjacentHTML('beforeend', footerMarkup);
 modalEl.insertAdjacentHTML('beforeend', modalMarkup);
 
-<<<<<<< Updated upstream
 const refs = {
   searchForm: document.querySelector('.js-search-form'),
   dataContainer: document.querySelector('.js-data-container'),
@@ -53,10 +52,3 @@ function onSearch(event) {
 function onLoadMore() {
   moviesApiService.fetchMovies();
 }
-=======
-fetch(
-  'https://api.themoviedb.org/3/movie/550?api_key=14d97542ae4a62e821967220e1ab473a',
-)
-  .then(r => r.json())
-  .then(console.log);
->>>>>>> Stashed changes
