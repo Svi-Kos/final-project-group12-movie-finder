77f297a9ad7e0ca4d851e8110d8006319bc75725
PasichnaDaryna <63604915+PasichnaDaryna@users.noreply.github.com>
2020-11-20 15:59:33 +0200

    переключение страниц

--- final-project-group12-movie-finder/src/index.js
+++ final-project-group12-movie-finder/src/index.js
@@ -14,9 +14,9 @@ import footer from './partials/footer.hbs';
 import modal from './templates/modal.hbs';
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
-// import { alert, defaultModules } from '@pnotify/core';
-// import '@pnotify/core/dist/PNotify.css';
-// import '@pnotify/core/dist/BrightTheme.css';
+import { alert, defaultModules } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
 
 const headerEl = document.querySelector('.header');
 const mainEl = document.querySelector('.main');
@@ -58,3 +58,48 @@ function appendFilms(results) {
   refs.dataContainer.insertAdjacentHTML('beforeend', cardFilmTpl(results));
   console.log(refs.dataContainer.offsetWidth);
 }
+
+
+
+// header buttons switches by Dasha
+
+const refsHeader
+  = {
+  
+  header: document.querySelector(".header"),
+  searchInfo: document.querySelector("#notify-text"),
+  searchIconRef: document.querySelector(".search-icon"),
+  pageHomeRef: document.querySelector(`[data-nav-choice="home"]`),
+  pageMyLibraryRef: document.querySelector(`[data-nav-choice="my-library"]`),
+  buttonListRef: document.querySelector(
+    `[data-button-list-header="watched-and-queue"]`
+  ),
+  inputSearchRef: document.querySelector(".search-field"),
+};
+
+
+
+
+
+refsHeader.pageMyLibraryRef.addEventListener("click", (event) => {
+  refsHeader.pageMyLibraryRef.classList.add("is-active");
+  refsHeader.pageHomeRef.classList.remove("is-active");
+
+  refsHeader.searchInfo.textContent = "";
+
+  refsHeader.buttonListRef.classList.remove("not-visible");
+  refsHeader.header.className = "header-lib";
+  refsHeader.inputSearchRef.classList.add("not-visible");
+  refsHeader.searchIconRef.classList.add("not-visible");
+});
+
+refsHeader.pageHomeRef.addEventListener("click", (event) => {
+  refsHeader.pageHomeRef.classList.add("is-active");
+  refsHeader.pageMyLibraryRef.classList.remove("is-active");
+
+  refsHeader.buttonListRef.classList.add("not-visible");
+  refsHeader.header.className = "header";
+
+  refsHeader.inputSearchRef.classList.remove("not-visible");
+  refsHeader.searchIconRef.classList.remove("not-visible");
+});
